130b18639d43db68a5a8a1db0fc4a59a7d126258
DesignerKnowsFrontEnd <s.kennedy@magloo.com>
2020-07-11 17:15:59 +0300

    got wordpress data to be pulled through :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +25,44 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    {
+      /////// Wordpress Plugin ///////
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'demo.wp-api.org' or 'www.example-site.com'
+         */
+        baseUrl: "wordpress-294785-1379784.cloudwaysapps.com",
+        // The protocol. This can be http or https.
+        protocol: "https",
+        // The rest api route prefix that your WordPress site is using.
+        // Sometimes this is modified by WordPress plugins.
+        // If not set, it uses the default of "wp-json"
+        restApiRoutePrefix: "wp-json",
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
+        // This feature is untested for sites hosted on wordpress.com.
+        // Defaults to true.
+        useACF: false,
+        // Include specific ACF Option Pages that have a set post ID
+        // Regardless if an ID is set, the default options route will still be retrieved
+        // Must be using V3 of ACF to REST to include these routes
+        // Example: `["option_page_1", "option_page_2"]` will include the proper ACF option
+        // routes with the ID option_page_1 and option_page_2
+        // The IDs provided to this array should correspond to the `post_id` value when defining your
+        // options page using the provided `acf_add_options_page` method, in your WordPress setup
+        // Dashes in IDs will be converted to underscores for use in GraphQL
+        acfOptionPageIds: [],
+        auth: {},
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems.
+        verboseOutput: true,
+      },
+    }
+  ]
 }
